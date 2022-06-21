885f99e9aeeae60f88af0bc54f76e1af390f3512
unknown <you@example.com>
2022-06-21 14:10:18 -0300

    Redsocial.py

--- Redsocial.py
+++ Redsocial.py
@@ -1,9 +1,15 @@
-Lista De las redes SocIales de los  Usuarios
 
-1. Instagram: https://www.instagram.com/debo_scheifler/
-2. Instagram:https://www.instagram.com/milisandoval.m/
-3. Instagram:https://www.instagram.com/lautarolaner/
-4. Instagram:https://www.instagram.com/carlosojeda93/ 
-5. Instagram:https://www.instagram.com/danielmerlo.1956/
-6. Instagram:https://www.instagram.com/alessagugiato/
-7. Instagram:https://www.instagram.com/ariiadna_navarro/
+Redes Sociales que manejamos:
+
+instagram: https://www.instagram.com/dulcecompania_ok/    
+
+
+Lista de Redes Sociales de los Usuarios
+
+1. Instagram: https://www.instagram.com/debo_scheifler/   Facebook: https://www.facebook.com/Debo.scheifler 
+2. Instagram: https://www.instagram.com/milisandoval.m/ 
+3. Instagram: https://www.instagram.com/lautarolaner/  
+4. Instagram: https://www.instagram.com/carlosojeda93/    Facebook: https://www.facebook.com/carlos.ojeda.92 
+5. Instagram: https://www.instagram.com/danielmerlo.1956/ Facebook: https://www.facebook.com/daniel.merlo.54 
+6. Instagram: https://www.instagram.com/alessagugiato/    Facebook https://www.facebook.com/alessandro.gugiato.7
+7. Instagram: https://www.instagram.com/ariiadna_navarro/ Facebook: https://www.instagram.com/ariiadna_navarro/
